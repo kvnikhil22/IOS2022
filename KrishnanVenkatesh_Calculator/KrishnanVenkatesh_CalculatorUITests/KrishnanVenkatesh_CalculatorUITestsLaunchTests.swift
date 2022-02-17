5e1043169aaf9fada68f3f97616a1e838366897b
kvnikhil22 <89282292+kvnikhil22@users.noreply.github.com>
2022-02-17 16:55:40 -0600

    Assignment_03

---
+++ KrishnanVenkatesh_Calculator/KrishnanVenkatesh_CalculatorUITests/KrishnanVenkatesh_CalculatorUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  KrishnanVenkatesh_CalculatorUITestsLaunchTests.swift
+//  KrishnanVenkatesh_CalculatorUITests
+//
+//  Created by Krishnan Venkatesh,Nikhil on 2/17/22.
+//
+
+import XCTest
+
+class KrishnanVenkatesh_CalculatorUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
