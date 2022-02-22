0e45356ce3c402b80a8d7ee1cb29a0673e78bbf9
kvnikhil22 <89282292+kvnikhil22@users.noreply.github.com>
2022-02-22 10:54:12 -0600

    wordApp

---
+++ WordGuessApp/WordGuessApp/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  WordGuessApp
+//
+//  Created by Krishnan Venkatesh,Nikhil on 2/22/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var stausLabel: UITextField!
+    @IBOutlet weak var displayLabel: UILabel!
+    @IBOutlet weak var playaginButton: UIButton!
+    @IBOutlet weak var checkButton: UIButton!
+    @IBOutlet weak var hintLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+  
+    @IBAction func clickCheckButton(_ sender: UIButton) {
+    }
+    @IBAction func playagainButton(_ sender: UIButton) {
+    }
+    @IBAction func enterLabel(_ sender: UITextField) {
+    }
+    
+}
+
